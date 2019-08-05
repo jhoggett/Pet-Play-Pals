a2465745faf448e08abd5234bbd8c665d0d1b5b0
Jacob Hoggett <hoggs660@gmail.com>
2019-08-05 14:14:04 -0400

    Committing changes to data file for SQL

--- mvc_dotnet/data.sql
+++ mvc_dotnet/data.sql
@@ -1,2 +1,2 @@
-﻿Insert into users (id, username, password, salt, role) 
-Values (1, test@gmail.com, TKC2qLTgrTA3DfeoCODvmkWxf5c=, BedDp8QY3XE=, User)
+﻿Insert into users (username, password, salt) 
+Values ('test@gmail.com', 'TKC2qLTgrTA3DfeoCODvmkWxf5c=', 'BedDp8QY3XE=')
