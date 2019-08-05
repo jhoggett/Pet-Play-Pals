61d881fbc1fb87f18408b4e58d6a1c01c66948c4
Michael Ambrozic <mike101390@gmail.com>
2019-08-05 16:00:09 -0400

    fixing the homepage and making it work

--- mvc_dotnet/data.sql
+++ mvc_dotnet/data.sql
@@ -1,2 +1,5 @@
 ﻿Insert into users (username, password, salt) 
-Values ('test@gmail.com', 'TKC2qLTgrTA3DfeoCODvmkWxf5c=', 'BedDp8QY3XE=')
+Values ('test@gmail.com', 'TKC2qLTgrTA3DfeoCODvmkWxf5c=', 'BedDp8QY3XE='),
+('definatelyEdSheeren@gmail.com', '++bSqEhHJrYoaqF/ItmAzDrRBoo=', 'qbZ25wcBKBI='),
+('rickybobby@yahoo.com', 'kwBulC00cwJuIIOy3RTEbnZ0OC0=', 'iSYnlKMQFDw='),
+('debra@gmail.com', 'b5cHlWa9K9rU2J3V9LmEOx+m0PE=', 'ek65kqNsyCM=')
