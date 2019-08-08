f94b497ecf5a155c3666882ff7bd0f289bc0d3c1
Jacob Hoggett <hoggs660@gmail.com>
2019-08-08 13:44:46 -0400

    Added more reservations in the data file for SQL

--- mvc_dotnet/data.sql
+++ mvc_dotnet/data.sql
@@ -12,7 +12,13 @@ values			 (4, 'Sammie', 'cat', 'Sassy little kitten who yells a lot', 1, 'tabby'
 				 (1, 'Pixie', 'dog', 'Well trained and loves other dogs', 57, 'Belgian Shepherd Dog Malinois', 3, 'http://res.cloudinary.com/petplaypals/image/upload/v1565200092/Pixie_rac3oi.jpg')
 			
 Insert into Reservations (address, startTime, endTime, petName)
-Values ('541 E. 266th St. Euclid Ohio 44132', '160819 12:00:00 PM', '160819 2:00:00 PM', 'Sammie')
+Values ('541 E. 266th St. Euclid Ohio 44132', '160819 12:00:00 PM', '160819 2:00:00 PM', 'Sammie'),
+('30419 Clarmont Rd. Willowick Ohio 44095', '180819 09:00:00 AM', '180819 12:00:00 PM', 'Chance'),
+('North Chagrin Reservation', '210819 02:00:00 PM', '210819 05:00:00 PM', 'Hulk'),
+('Sim''s Park', '230819 11:30:00 AM', '230819 12:30:00 PM', 'Pixie')
 
 Insert into Users_Reservations (userId, reservationId)
-Values (4, 1)	 
+Values (4, 1),	
+(4, 2),
+(3, 3),
+(1, 4) 
